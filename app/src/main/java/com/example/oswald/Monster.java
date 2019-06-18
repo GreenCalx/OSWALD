f5cd5ebf756638d423e08cb5b7f0d62ef92f2ced
GreenCalx <romain.schoenberger@gmail.com>
2019-06-18 23:26:41 +0200

    3foisri1 @GreenCalx

--- app/src/main/java/com/example/oswald/Monster.java
+++ app/src/main/java/com/example/oswald/Monster.java
@@ -1,7 +1,5 @@
 package com.example.oswald;
 
-import android.util.Pair;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -9,45 +7,45 @@ import java.util.List;
 
 public class Monster {
 
-    enum EN_SKILLS
+    enum enumSKILLS
     { COMBAT, SUBTERFUGE, MYSTICAL, TRICKERY }
-    enum EN_STATS
+    enum enumSTATS
     { HP, EN, SAVE }
 
     /// GENERATIVE STATS
     public int                          FD      ;// difficulty of the creature
     public int                          LVL     ;// toughness of the creature
-    public HashMap<EN_STATS,Integer>       COUNTERS;// HP/Energy counter repartition
-    public HashMap<EN_SKILLS,Integer>      SKILLS  ;// Skills with levels ( I , II, II, III )
+    public HashMap<enumSTATS,Integer>   COUNTERS;// HP/Energy counter repartition
+    public HashMap<enumSKILLS,Integer>  ORDERED_SKILLS  ;// Skills with levels ( I , II, II, III )
 
     /// STATS
-    public int                  health;
-    public int                  energy;
-    public int                  save;
-    List<MonsterAttribute>      attributes;
-    HashMap<EN_SKILLS, Integer>   skills;
+    public int                      health;
+    public int                      energy;
+    public int                      save;
+    List<MonsterAttribute>          attributes;
+    HashMap<enumSKILLS, Integer>    skills;
 
     /// LORE
     public String   name;
     public String   description;
 
 
-    public Monster( int iFD, int iLVL, HashMap<EN_STATS,Integer> iCounters, HashMap<EN_SKILLS,Integer> iSkills)
+    public Monster(int iFD, int iLVL, HashMap<enumSTATS,Integer> iCounters, HashMap<enumSKILLS,Integer> iSkills)
     {
         FD          = iFD;
         LVL         = iLVL;
         COUNTERS    = iCounters;
-        SKILLS      = iSkills;
+        ORDERED_SKILLS      = iSkills;
     }
 
     public void generate()
     {
         save    = FD + LVL + 10;
-        health  = FD * LVL * COUNTERS.get(EN_STATS.HP);
-        energy  = FD * LVL * COUNTERS.get(EN_STATS.EN);
+        health  = FD * LVL * COUNTERS.get(enumSTATS.HP);
+        energy  = FD * LVL * COUNTERS.get(enumSTATS.EN);
 
         skills = new HashMap<>(4);
-        Arrays.asList( EN_SKILLS.values() ).forEach( skill -> skills.put( skill, SKILLS.get(skill) ));
+        Arrays.asList( enumSKILLS.values() ).forEach(skill -> skills.put( skill, ORDERED_SKILLS.get(skill) ));
 
         attributes = new ArrayList<>( FD * 2 );
     }
